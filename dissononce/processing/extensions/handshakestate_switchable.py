00cff6f3b97b7b103dfc11236f653cee519fca64
Tarek Galal <tare2.galal@gmail.com>
2019-03-15 12:13:59 +0100

    [feat] add SwitchableHandshakeState extension
    allows switching an ongoing Handshake to a different pattern,
    taking over those already initialized variables which are also
    part of the new Handshake Pattern pre-messages

---
+++ dissononce/processing/extensions/handshakestate_switchable.py
@@ -0,0 +1,45 @@
+from dissononce.processing.extensions.handshakestate_forwarder import ForwarderHandshakeState
+
+
+class SwitchableHandshakeState(ForwarderHandshakeState):
+    def __init__(self, handshakestate):
+        """
+        :param handshakestate:
+        :type handshakestate: HandshakeState
+        """
+        super(SwitchableHandshakeState, self).__init__(handshakestate)
+
+    def switch(self, handshake_pattern, initiator, prologue, s, e=None, rs=None, re=None):
+        """
+        :param handshake_pattern:
+        :type handshake_pattern: HandshakePattern
+        :param initiator:
+        :type initiator: bool
+        :param prologue:
+        :type prologue:
+        :param s:
+        :type s:
+        :param e:
+        :type e:
+        :param rs:
+        :type rs:
+        :param re:
+        :type re:
+        :return:
+        :rtype:
+        """
+        for pattern in handshake_pattern.initiator_pre_message_pattern:
+            for token in pattern:
+                if token == 'e':
+                    e = self.e
+                if token == 's':
+                    s = self.s
+
+        for pattern in handshake_pattern.responder_pre_message_pattern:
+            for token in pattern:
+                if token == 'e':
+                    re = self.re
+                if token == 's':
+                    rs = self.rs
+
+        return self.initialize(handshake_pattern, initiator, prologue, s, e, rs, re)
