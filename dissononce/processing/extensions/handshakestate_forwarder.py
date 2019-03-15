8d302c20feb8bf3ea8c865e13de31a34352d6de5
Tarek Galal <tare2.galal@gmail.com>
2019-03-15 12:12:02 +0100

    [feat] add ForwardedHandshakeState extension
    This just forwards all calls to an underlying HandshakeState.
    HandshakeState extensions that wish to partially modify a HandshakeState
    implementation should subclass this for less boilerplate code.

---
+++ dissononce/processing/extensions/handshakestate_forwarder.py
@@ -0,0 +1,35 @@
+from dissononce.processing.handshakestate import HandshakeState
+
+
+class ForwarderHandshakeState(HandshakeState):
+    def __init__(self, handshakestate):
+        """
+        :param handshakestate:
+        :type handshakestate: HandshakeState
+        """
+        self._handshakestate = handshakestate
+
+    def initialize(self, handshake_pattern, initiator, prologue, s, e=None, rs=None, re=None):
+        return self._handshakestate.initialize(handshake_pattern, initiator, prologue, s, e, rs, re)
+
+    def write_message(self, payload, message_buffer):
+        return self._handshakestate.write_message(payload, message_buffer)
+
+    def read_message(self, message, payload_buffer):
+        return self._handshakestate.read_message(message, payload_buffer)
+
+    @property
+    def re(self):
+        return self._handshakestate.re
+
+    @property
+    def rs(self):
+        return self._handshakestate.rs
+
+    @property
+    def s(self):
+        return self._handshakestate.s
+
+    @property
+    def e(self):
+        return self._handshakestate.e
