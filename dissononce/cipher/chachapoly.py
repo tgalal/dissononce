f1a4d8446db885b42e94520fa5a9853da9c58ee7
Tarek Galal <tare2.galal@gmail.com>
2019-03-26 05:09:06 +0100

    [feat] add chachapoly cipher

---
+++ dissononce/cipher/chachapoly.py
@@ -0,0 +1,18 @@
+from dissononce.cipher.cipher import Cipher
+
+from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
+
+
+class ChaChaPolyCipher(Cipher):
+    def __init__(self):
+        super(ChaChaPolyCipher, self).__init__("ChaChaPoly")
+
+    def encrypt(self, key, nonce, ad, plaintext):
+        return ChaCha20Poly1305(key).encrypt(self.__class__._format_nonce(nonce), plaintext, ad)
+
+    def decrypt(self, key, nonce, ad, ciphertext):
+        return ChaCha20Poly1305(key).decrypt(self.__class__._format_nonce(nonce), ciphertext, ad)
+
+    @staticmethod
+    def _format_nonce(n):
+        return b'\x00\x00\x00\x00' + n.to_bytes(length=8, byteorder='little')
