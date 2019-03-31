fc84568a7327ed44659c9703e4360bdbb8dffb4d
Tarek Galal <tare2.galal@gmail.com>
2019-03-31 12:40:01 +0200

    [doc] update hash functions and props docs

--- dissononce/hash/hash.py
+++ dissononce/hash/hash.py
@@ -1,8 +1,13 @@
 class Hash(object):
     def __init__(self, name, hashlen, blocklen):
         """
-        :param name:
+        :param name: Name of hash function algorithm
         :type name: str
+        :param hashlen: HASHLEN = size in bytes of the hash output. Must be 32 or 64.
+        :type hashlen: int
+        :param blocklen: BLOCKLEN = size in bytes that the hash function uses internally to divide its input for
+        iterative processing. This is needed to use the hash function with HMAC
+        :type blocklen: int
         """
         assert hashlen in (32, 64), "Unsupported hashlen %d" % hashlen
         self._name = name  # type: str
@@ -22,17 +27,52 @@ class Hash(object):
         return self._blocklen
 
     def hash(self, data):
-        '''
+        """
+        Hashes some arbitrary-length data with a collision-resistant cryptographic
+        hash function and returns an output of HASHLEN bytes.
         :param data:
         :type data: bytes
         :return:
         :rtype: bytes
+        """
 
     def hmac_hash(self, key, data):
+        """
+        HMAC-HASH(key, data)
+        Applies HMAC using the HASH() function. This function is only called as part of HKDF()
+        :param key:
+        :type key: bytes
+        :param data:
+        :type data: bytes
+        :return:
+        :rtype: bytes
+        """
         pass
 
-
     def hkdf(self, chaining_key, input_key_material, num_outputs):
+        """
+        HKDF(chaining_key, input_key_material, num_outputs)
+        Takes a chaining_key byte sequence of length HASHLEN, and an input_key_material byte sequence with length
+        either zero bytes, 32 bytes, or DHLEN bytes.
+        Returns a pair or triple of byte sequences each of length HASHLEN, depending on whether num_outputs is
+        two or three
+
+        Sets temp_key = HMAC-HASH(chaining_key, input_key_material).
+        Sets output1 = HMAC-HASH(temp_key, byte(0x01)).
+        Sets output2 = HMAC-HASH(temp_key, output1 || byte(0x02)).
+        If num_outputs == 2 then returns the pair (output1, output2).
+        Sets output3 = HMAC-HASH(temp_key, output2 || byte(0x03)).
+        Returns the triple (output1, output2, output3).
+
+        :param chaining_key:
+        :type chaining_key: bytes
+        :param input_key_material:
+        :type input_key_material: bytes
+        :param num_outputs:
+        :type num_outputs: int
+        :return:
+        :rtype: tuple
+        """
         temp_key = self.hmac_hash(chaining_key, input_key_material)
 
         # Sets output1 = HMAC-HASH(temp_key, byte(0x01)).
@@ -50,5 +90,3 @@ class Hash(object):
 
         # Returns the triple (output1, output2, output3).
         return output1, output2, output3
-
-
