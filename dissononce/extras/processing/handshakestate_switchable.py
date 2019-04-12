9df9a9912acccc79451fdc24bdf50f0dd4069aff
Tarek Galal <tare2.galal@gmail.com>
2019-04-12 15:34:06 +0200

    [fix] switch with correct responder handshake vars
    SwitchableHandshakeState did not maintain correct variables when the responder
    switched to a different pattern

--- dissononce/extras/processing/handshakestate_switchable.py
+++ dissononce/extras/processing/handshakestate_switchable.py
@@ -10,18 +10,33 @@ class SwitchableHandshakeState(ForwarderHandshakeState):
         super(SwitchableHandshakeState, self).__init__(handshakestate)
 
     def switch(self, handshake_pattern, initiator, prologue, s=None, e=None, rs=None, re=None, psks=None):
-        for pattern in handshake_pattern.initiator_pre_message_pattern:
-            for token in pattern:
-                if token == 'e':
-                    e = self.e
-                if token == 's':
-                    s = self.s
+        if initiator:
+            for pattern in handshake_pattern.initiator_pre_message_pattern:
+                for token in pattern:
+                    if token == 'e':
+                        e = self.e
+                    if token == 's':
+                        s = self.s
 
-        for pattern in handshake_pattern.responder_pre_message_pattern:
-            for token in pattern:
-                if token == 'e':
-                    re = self.re
-                if token == 's':
-                    rs = self.rs
+            for pattern in handshake_pattern.responder_pre_message_pattern:
+                for token in pattern:
+                    if token == 'e':
+                        re = self.re
+                    if token == 's':
+                        rs = self.rs
+        else:
+            for pattern in handshake_pattern.initiator_pre_message_pattern:
+                for token in pattern:
+                    if token == 'e':
+                        re = self.re
+                    if token == 's':
+                        rs = self.rs
+
+            for pattern in handshake_pattern.responder_pre_message_pattern:
+                for token in pattern:
+                    if token == 'e':
+                        e = self.e
+                    if token == 's':
+                        s = self.s
 
         return self.initialize(handshake_pattern, initiator, prologue, s, e, rs, re, psks)
